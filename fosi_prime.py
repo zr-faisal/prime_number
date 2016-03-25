2fc22a905cd9571ce5db39044027b51e4f829c79
Zahidur Rahman Faisal <zr.faisal@programmer.net>
2016-03-25 09:54:14 +0600

    Update fosi_prime.py

--- fosi_prime.py
+++ fosi_prime.py
@@ -3,6 +3,8 @@ import math
 
 # Detecting if the given value is prime number by checking it from 2 to it's squre-root
 def is_prime(value):
+	if value == 1:
+			return False
 	for i in xrange(2, int(math.sqrt(value) + 1)):
 		if value%i == 0:
 			return False
@@ -10,13 +12,13 @@ def is_prime(value):
 
 # Printing prime numbers from given minimum to maximum value
 def find_primes_in_range(range_min, range_max):
-	prime_list = []
-	for i in xrange(range_min, range_max):
+	count = 0
+	for i in xrange(range_min, range_max+1):
 		if is_prime(i):
-			prime_list.append(i)
+			print i
+			count += 1
 	# Print the list of primes
-	print "Total %d Prime Numbers between %d and %d inclusive. They are:\n%r" \
-		%(len(prime_list), range_min, range_max, prime_list)
+	print "Total %d Prime Numbers between %d and %d inclusive." %(count, range_min, range_max)
 
 if __name__ == '__main__':
 	find_primes_in_range(19900, 20000)
